aa6ff0d49e8bf577b951c933cc31d1d80ebd7582
XpeHo3eM <npv90@mail.ru>
2021-02-18 10:52:00 +0400

    Добавьте файлы проекта.

---
+++ 22_1_task.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::cout << "Enter the word you want to find: ";
+    std::string word;
+    std::cin >> word;
+
+    std::fstream file;
+    file.open("words.txt");
+
+    std::string temp;
+    int count = 0;
+    while (!file.eof())
+    {
+        file >> temp;
+        if (word == temp)
+            ++count;
+    }
+    std::cout << count << std::endl;
+    file.close();
+}
